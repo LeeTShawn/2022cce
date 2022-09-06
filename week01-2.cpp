e1dda8f77325888c34f1079038a084f4d74d81fa
LeeTShawn <112918703+LeeTShawn@users.noreply.github.com>
2022-09-06 11:30:57 +0800

    Add files via upload

---
+++ week01-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("我是李宗軒\n");
+    ///有時候中文會有問題,你可以用英文
+    //註解comments是給人看的 作業2加註解
+    ///CodeBlocks如果畫3條,會比較深
+}
